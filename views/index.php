6e3485d28115eb67d62ecaee4d73d88a4bc1776c
Nick <niccamillo@gmail.com>
2017-12-11 10:44:08 -0500

    test commit

--- views/index.php
+++ views/index.php
@@ -12,7 +12,9 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        
+        ?>
+        <div>
+        </div>
         ?>
     </body>
 </html>
