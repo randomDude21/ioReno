636f7cd30864b8fa81fb0da89d73d9780b7da25d
Gleeky <34449927+Gleeky@users.noreply.github.com>
2017-12-13 13:40:55 -0500

    Update HomeContractor.php

--- views/HomeContractor.php
+++ views/HomeContractor.php
@@ -10,6 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        https://v4-alpha.getbootstrap.com/components/card/
         <?php
         // put your code here
         ?>
