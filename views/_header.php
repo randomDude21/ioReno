9baa2493e6d13757de1224a89e62b15acf18d2af
Nick <niccamillo@gmail.com>
2017-12-13 09:29:50 -0500

    index page work

--- views/_header.php
+++ views/_header.php
@@ -7,6 +7,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link href="../css/ioStyle.css" rel="stylesheet" type="text/css"/>
+  <script src="../js/gen_validatorv4.js" type="text/javascript"></script>
         <title>IOReno</title>
     </head>
     <body>
