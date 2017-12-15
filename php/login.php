908d55cc4386e4bdde59abc69ddc8693d28fa6a1
randomDude21 <alex_boy777@hotmail.com>
2017-12-15 09:55:52 -0500

    Login functionality now works for customers

--- php/login.php
+++ php/login.php
@@ -1,13 +1,19 @@
 <?php
 require "../Classes/Database.php";
-
+session_start();
 $db=new Database();
 if($db->getCustomerE($_POST['Email'])!= null)
 {
     $customer=$db->getCustomerE($_POST['Email']);
     if($customer->get_password()== sha1($_POST['Password']))
     {
+        $_SESSION["message"]="";
         header("location:../views/HomeCustomer.php");
+    }
+    else
+    {
+        $_SESSION["message"]="Invalid login attemp";
+        header("location:../views/index.php");
     }
 }
 else if($db->getContractorE($_POST['Email'])!= null)
@@ -15,11 +21,18 @@ else if($db->getContractorE($_POST['Email'])!= null)
     $contractor=$db->getContractorE($_POST['Email']);
     if ($contractor->get_password()== sha1($_POST['Password']))
     {
+        $_SESSION["message"]="";
         header("location:../views/HomeContractor.php");
+    }
+    else
+    {
+        $_SESSION["message"]="Invalid login attemp";
+        header("location:../views/index.php");
     }
 }
 else
 {
-    setcookie("message", "Invalid login attemp", time()+7);
+    $_SESSION["message"]="Invalid login attemp";
+    header("location:../views/index.php");
 }
 ?>
