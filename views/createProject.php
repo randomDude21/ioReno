a33c47af31f03b55ce70935625c92e91031fd7fd
1031275 <1031275@F249-06.champlaincollege.qc.ca>
2018-01-12 16:24:15 -0500

    sticky forms for customer and contractor signup working

---
+++ views/createProject.php
@@ -0,0 +1,15 @@
+<?php
+    include("_header.php");
+    if (!$_SESSION["login"]){
+        header("Location: index.php");
+    }
+?>
+
+<div class="container-fluid">
+    <h1>Post a Project</h1>
+    <
+</div>
+
+<?php
+    include("_footer.php");
+?>
