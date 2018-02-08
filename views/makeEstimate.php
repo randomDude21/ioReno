78c2513fe10b622c3e47d0b7f2757892c8c68589
randomDude21 <alex_boy777@hotmail.com>
2018-02-08 15:57:16 -0500

    makeestimate is better

--- views/makeEstimate.php
+++ views/makeEstimate.php
@@ -15,31 +15,35 @@ if (!$_SESSION["login"])
 $project = $db->getProject($_GET["id"]);
 $_SESSION["project"] = $_GET["id"];
 ?>
-<div class="container-fluid">
-    <div class="col-sm-8">
+<div class="container-fluid row" style="width: 85%; margin-left: auto; margin-right: auto; margin-top: 1%">
+    <div class="col-sm-6">
         
-        <h2><?php echo $project->getTitle(); ?></h2>
-        <h3>Budget</h3>
+        <h3><strong><?php echo $project->getTitle(); ?></strong></h3>
+        <h4>Budget</h4>
             <span>$<?php echo $project->get_budget(); ?></span>
         <div><img class="view-img" src="<?php echo $project->getImages() ?>" width="82.5%"></div>
-        <h3>Description</h3>
+        <h4>Project Type</h4>
+        <div class="description"><?php echo $project->get_type();?></div>
+        <h4>Description</h4>
         <div class="description"><?php echo $project->get_description(); ?></div>
+   
+        <h4>Address</h4>
+        <div class="description"><?php echo $project->getAddress(); ?></div>
+         <a href="HomeContractor.php" class="btn btn-danger" role="button"><strong>Go back</strong></a>
     </div>
-    <div class="col-sm-6">
-        <h3>Address</h3>
-    </div>
-    
-        <h4>Make Estimate</h4>
+    <div class="col-sm-6" style="margin-top: 3%">
+    <h3>Make Estimate</h3>
         <form action="../php/confirmEstimate.php" method="POST">
             <input type="number" id="estimate" name="estimate">
             <span name="error"><?php if (isset($_SESSION['error'])) echo("{$_SESSION['error']}"); $_SESSION['error'] = ""; ?></span>
             
     
     <div style="margin-top: 20px">
-        <a href="HomeContractor.php" class="btn btn-danger" role="button"><strong>Go back</strong></a>
+       
         <input type="submit" class="btn btn-success" value="Submit">
         </form>
         </div>
+    </div>
 
 
 </div>
