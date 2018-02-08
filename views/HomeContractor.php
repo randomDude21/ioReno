96d9b0390df8b6be50238e6f7b4698551cf5092b
1031275 <1031275@F249-06.champlaincollege.qc.ca>
2018-02-08 15:14:19 -0500

    added sorting by type on homeContractor

--- views/HomeContractor.php
+++ views/HomeContractor.php
@@ -10,7 +10,15 @@ if (!$_SESSION["login"]){
         $_SESSION["update"] = null;
     }
     $db=new Database();
-    $projects = $db->getProjects();
+    if (isset($_GET["type"])) {
+        
+        $projects = $db->sortProjects($_GET["type"]);
+    }
+    else {
+        $projects = $db->getProjects();
+    }
+    
+    
     ?>
 <div class="container-fluid">
     <a href="pastEstimates.php" class="btn btn-primary">See your estimates</a>
@@ -21,6 +29,31 @@ if($projects!=null)
 ?>
 <div class="container-fluid" style="width:85%">
     <h1>Project Listings</h1>
+        <div>
+            <h3>Project Types</h3>
+            <form action="HomeContractor.php" method="get">
+                <ul class="nav flex-column">
+                    <?php
+                        include("../Classes/ProjectTypes.php");
+                        foreach ($projectTypes as $val) {
+                            ?>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">
+                                        <input type="checkbox" name="type[]" value="<?php echo $val; ?>" <?php if (isset($_GET["type"]) && in_array($val, $_GET["type"])) echo "checked=chekced"; ?>>
+                                        <label for="<?php echo $val; ?>"><?php echo $val ?></label>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            <?php
+                        }
+                    ?>
+                </ul>
+                <input type="submit" value="Search">
+                <input type="reset" value="Clear">
+            </form>
+        </div>
     <?php
         foreach ($projects as $pro)
         { 
