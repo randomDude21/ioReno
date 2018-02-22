c02dcba6eab36de7dc08f308d295afd9e6a5d90d
Nick <Nick@GANJA-5000>
2018-02-21 22:50:54 -0500

    added terms page

---
+++ views/terms.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    include("_header.php");
+?>
+<div class="container-fluid">
+    <h1>Terms of Use</h1>
+    <p>Coming soon</p>
+</div>
+<?php
+    include("_footer.php");
+?>
